ccce47a1d14b7adbafc2c1cb5f110eee781d8873
Thoreeq16 <thoriqhidayatullah16@gmail.com>
2022-10-06 18:42:31 +0700

    Class program

--- Act1BUBBLESORT/Program.cs
+++ Act1BUBBLESORT/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using System;
+
+namespace Bubble_Sort
+{
+    class program
+    {
+        // Deklarasi array int dengan ukuran 20
+        private int[] a = new int[20];
+
+        // Deklarasi variabel int untuk menyimpan banyaknya data pada array
+        private int n;
